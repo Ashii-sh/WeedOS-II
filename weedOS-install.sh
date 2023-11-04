c444c0c2496a5e4b2b10789a956dec8ff146ca54
Ashii-sh <149899656+Ashii-sh@users.noreply.github.com>
2023-11-04 18:10:31 -0400

    Create weedOS-install.sh

---
+++ weedOS-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo 'Weed OS II 1.0 Main' 
+echo 'Made with < 3 in Florida'
+sleep 3
+clear
+
+### Stage 1 
+
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install xserver-xorg xinit htop mesa-utils firmware-realtek firmware-iwlwifi git unzip wget plymouth plymouth-themes cpufrequtils lightdm xserver-xorg-input-synaptics -y
+mkdir cache 
+
+### Stage 2 
+
+sudo apt install xfce4 xfce4-whiskermenu-plugin picom plank qt5-gtk2-platformtheme network-manager network-manager-gnome xfce4-power-manager-plugins xfce4-whiskermenu-plugin -y
+sudo apt install numix-icon-theme-circle numix-icon-theme fonts-roboto menulibre mugshot lightdm-gtk-greeter-settings xfce4-screenshooter mousepad 
+cd cache
+git clone https://github.com/numixproject/numix-icon-theme-circle
+sudo cp -r numix-icon-theme-circle/Numix-* /usr/share/icons
+cp -r numix-icon-theme-circle/Numix-* ~/.icons
+rm -rf numix-icon-theme-circle/
+wget https://github.com/catppuccin/gtk/releases/download/v0.7.0/Catppuccin-Frappe-Standard-Green-Dark.zip
+wget https://github.com/catppuccin/gtk/releases/download/v0.7.0/Catppuccin-Latte-Standard-Green-Light.zip
+unzip Catppuccin-*.zip
+rm Catppuccin-*.zip
+cp -r Catppuccin-* ~/.themes
+rm -rf Catppuccin-* 
