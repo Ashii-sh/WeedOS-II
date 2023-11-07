5f7a603cdfe33d7e16b835f203cf83deff50e0bc
Ashii-sh <149899656+Ashii-sh@users.noreply.github.com>
2023-11-07 16:24:43 -0500

    1.7b - Fixing QT5 theme

--- weedOS-install-beta.sh
+++ weedOS-install-beta.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'Weed OS II 1.6 Beta' 
+echo 'Weed OS II 1.7 Beta' 
 echo 'Made with < 3 in Florida'
 sleep 3
 clear
@@ -10,11 +10,21 @@ sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install xserver-xorg xinit htop mesa-utils firmware-realtek firmware-iwlwifi git unzip wget plymouth plymouth-themes cpufrequtils lightdm xserver-xorg-input-synaptics -y
 mkdir cache 
+mkdir -p "${HOME}/.config/gtk-4.0"
+mkdir .icons
+mkdir .themes
+mkdir Desktop
+mkdir Downloads
+mkdir Documents
+mkdir Pictures
+mkdir Videos 
+mkdir ~/.config/qt5ct/colors
 
 ### Stage 2 
 
 sudo apt install xfce4 xfce4-whiskermenu-plugin picom plank qt5-gtk2-platformtheme network-manager network-manager-gnome xfce4-power-manager-plugins xfce4-whiskermenu-plugin -y
-sudo apt install numix-icon-theme-circle numix-icon-theme fonts-roboto menulibre mugshot lightdm-gtk-greeter-settings xfce4-screenshooter mousepad -y
+sudo apt install numix-icon-theme-circle numix-icon-theme fonts-roboto menulibre mugshot lightdm-gtk-greeter-settings xfce4-screenshooter mousepad qt5ct -y
+WEEDOSSCRIPT=`pwd`
 cd cache
 git clone https://github.com/numixproject/numix-icon-theme-circle
 sudo cp -r numix-icon-theme-circle/Numix-* /usr/share/icons
@@ -22,26 +32,28 @@ cp -r numix-icon-theme-circle/Numix-* ~/.icons
 rm -rf numix-icon-theme-circle/
 wget https://github.com/catppuccin/gtk/releases/download/v0.7.0/Catppuccin-Frappe-Standard-Green-Dark.zip
 wget https://github.com/catppuccin/gtk/releases/download/v0.7.0/Catppuccin-Latte-Standard-Green-Light.zip
-mkdir ~/.themes
 unzip Catppuccin-Frappe-Standard-Green-Dark.zip
 unzip Catppuccin-Latte-Standard-Green-Light.zip
 rm Catppuccin-*.zip
-cp -r Catppuccin-* ~/.themes
+cp -r Catppuccin-* ~/.themes/
+sudo cp -r Catppuccin-* /usr/share/themes
 rm -rf Catppuccin-* 
-# GTK 4 Fix 
-mkdir -p "${HOME}/.config/gtk-4.0"
-#ln -sf "${THEME_DIR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets"
-#ln -sf "${THEME_DIR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css"
-#ln -sf "${THEME_DIR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
-
-# GTK 4 Fix didnt work, assumingly due to the ${THEME_DIR} variable not being set to anything.
-
-# Better fix supposedly V V V 
-#cd ~/.config/xfce4/xfconf/xfce-perchannel-xml
-#THEME_VAR=`cat xfwm4.xml | grep Catppuccin | cut -cX-Y `
-#export THEME_VAR
-#ln -sf "${THEME_VAR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets"
-#ln -sf "${THEME_VAR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css"
-#ln -sf "${THEME_VAR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
-#Replace X and Y for the actual values of the location of Catppuccin-Green-Lite/Dark
-#These commans may need to become a startup script, will come back to later.
+cd ~/.themes/
+mv Catppuccin-Frappe-Standard-Green-Dark Catppuccin-Green-Dark
+mv Catppuccin-Latte-Standard-Green-Light Catppuccin-Green-Lite
+cd /usr/share/themes/
+sudo mv Catppuccin-Frappe-Standard-Green-Dark Catppuccin-Green-Dark
+sudo mv Catppuccin-Latte-Standard-Green-Light Catppuccin-Green-Lite
+cd ${WEEDOSSCRIPT}/ 
+#cd cache 
+#sudo apt install cmake extra-cmake-modules libkdecorations2-dev libkf5plasma-dev libqt5x11extras5-dev qtdeclarative5-dev libkf5windowsystem-dev libkf5coreaddons-dev libkf5iconthemes-dev gettext qt3d5-dev build-essential libkf5guiaddons-dev libkf5configwidgets-dev -y
+#git clone --single-branch --depth=1 https://github.com/Luwx/Lightly.git
+#cd Lightly && mkdir build && cd build
+#cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF ..
+#make
+#sudo make install
+#cd ..
+#cd ..
+#git clone https://github.com/catppuccin/qt5ct
+#sudo cp /qt5ct/Catppuccin-Frappe.conf ~/.config/qt5ct/colors/ 
+#sudo cp /qt5ct/Catppuccin-Latte.conf ~/.config/qt5ct/colors/ 
