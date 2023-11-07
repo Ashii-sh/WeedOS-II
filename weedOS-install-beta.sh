62111eebab882dcd4cb423221ca6ed3175effa40
Ashii-sh <149899656+Ashii-sh@users.noreply.github.com>
2023-11-06 22:25:03 -0500

    II 1.4 Beta - Fixed .themes folder creation.

--- weedOS-install-beta.sh
+++ weedOS-install-beta.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'Weed OS II 1.3 Beta' 
+echo 'Weed OS II 1.4 Beta' 
 echo 'Made with < 3 in Florida'
 sleep 3
 clear
@@ -22,6 +22,7 @@ cp -r numix-icon-theme-circle/Numix-* ~/.icons
 rm -rf numix-icon-theme-circle/
 wget https://github.com/catppuccin/gtk/releases/download/v0.7.0/Catppuccin-Frappe-Standard-Green-Dark.zip
 wget https://github.com/catppuccin/gtk/releases/download/v0.7.0/Catppuccin-Latte-Standard-Green-Light.zip
+mkdir ~/.themes
 unzip Catppuccin-Frappe-Standard-Green-Dark.zip
 unzip Catppuccin-Latte-Standard-Green-Light.zip
 rm Catppuccin-*.zip
