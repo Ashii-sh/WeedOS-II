b74fc4cdf56b339e0659107313a7bb141cd8a66f
Ashii-sh <149899656+Ashii-sh@users.noreply.github.com>
2023-11-07 22:05:33 -0500

    1.9.4b - Global Menu Addition

--- weedOS-install-beta.sh
+++ weedOS-install-beta.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'Weed OS II 1.9.3 Beta' 
+echo 'Weed OS II 1.9.4 Beta' 
 echo 'Made with < 3 in Florida'
 sleep 3
 clear
@@ -9,6 +9,7 @@ clear
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install xserver-xorg xinit htop mesa-utils firmware-realtek firmware-iwlwifi git unzip wget plymouth plymouth-themes cpufrequtils lightdm xserver-xorg-input-synaptics -y
+
 mkdir cache 
 mkdir -p "${HOME}/.config/gtk-4.0"
 mkdir ~/.icons
@@ -26,11 +27,13 @@ sudo apt install xfce4 xfce4-whiskermenu-plugin picom plank qt5-gtk2-platformthe
 sudo apt install numix-icon-theme-circle numix-icon-theme fonts-roboto menulibre mugshot lightdm-gtk-greeter-settings xfce4-screenshooter mousepad qt5ct -y
 sudo apt install firefox-esr gnome-software flatpak gnome-software-plugin-flatpak vlc -y
 WEEDOSSCRIPT=`pwd`
+
 cd cache
 git clone https://github.com/numixproject/numix-icon-theme-circle
 sudo cp -r numix-icon-theme-circle/Numix-* /usr/share/icons
 cp -r numix-icon-theme-circle/Numix-* ~/.icons
 rm -rf numix-icon-theme-circle/
+
 wget https://github.com/catppuccin/gtk/releases/download/v0.7.0/Catppuccin-Frappe-Standard-Green-Dark.zip
 wget https://github.com/catppuccin/gtk/releases/download/v0.7.0/Catppuccin-Latte-Standard-Green-Light.zip
 unzip Catppuccin-Frappe-Standard-Green-Dark.zip
@@ -50,7 +53,7 @@ cd ${WEEDOSSCRIPT}/
 ### Stage 3 
 
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
-#wget < USER CONF >
+
 cd cache
 mkdir theme
 cd theme
@@ -68,5 +71,12 @@ cd cache
 sudo cp -r theme/* /usr/share/desktop-base/emerald-theme/wallpaper/
 cd ${WEEDOSSCRIPT}/
 
+cd cache
+git clone https://github.com/HeteroChromia420/Global-Menu-Enabler
+sudo apt install xfce4-appmenu-plugin vala-panel-appmenu appmenu* -y 
+xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
+xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
+sudo cp Global-Menu-Enabler/52appmenu-gtk-module_add-to-gtk-modules /etc/X11/Xsession.d
 
+#wget <USER DATA>
 
