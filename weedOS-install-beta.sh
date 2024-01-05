510dc3d9ad1dc8f9b18c5bb73caaacbe0498088a
Ashii-sh <149899656+Ashii-sh@users.noreply.github.com>
2024-01-05 03:34:29 -0500

    Update weedOS-install-beta.sh - Beta 2.0.1
    -Polkit fix

--- weedOS-install-beta.sh
+++ weedOS-install-beta.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'Weed OS II 2.0.0 Beta' 
+echo 'Weed OS II 2.0.1 Beta' 
 echo 'Made with < 3 in Florida'
 sleep 3
 clear
@@ -104,6 +104,8 @@ mkdir homefolder
 cd homefolder
 wget https://cdn.discordapp.com/attachments/926440184411729940/1188717524209119232/UserConfigFolder-12-24-2023.zip
 cd.. 
+wget https://cdn.discordapp.com/attachments/926440184411729940/1188717523923914792/org.freedesktop.login1.policy
+sudo cp -r org.freedesktop.login1.policy /usr/share/polkit-1/
 cp -r homefolder/* ${HOME}/
 cd..
 
